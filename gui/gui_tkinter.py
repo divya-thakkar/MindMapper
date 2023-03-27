dc290759e133d6f0bb4c0d5592d61148804e2c46
divya-thakkar <thakkardivya17@gmail.com>
2023-03-27 03:47:51 -0400

    volume buttons and meter functionality

--- gui/gui_tkinter.py
+++ gui/gui_tkinter.py
@@ -16,6 +16,25 @@ videoplayer.pack(expand=True, fill="both")
 mixer.init()
 mixer.music.load("./assets/iCarly_scene_compressed.mp3")
 
+master_frame = Frame(window)
+master_frame.pack(pady=20)
+
+global vol0
+global vol1
+global vol2
+global vol3
+global vol4
+vol0 = PhotoImage(file='./assets/images/volume0.png')
+vol1 = PhotoImage(file='./assets/images/volume1.png')
+vol2 = PhotoImage(file='./assets/images/volume2.png')
+vol3 = PhotoImage(file='./assets/images/volume3.png')
+vol4 = PhotoImage(file='./assets/images/volume4.png')
+
+# Create Volume Meter
+mixer.music.set_volume(0.625)
+volume_meter = Label(master_frame, image=vol3)
+volume_meter.pack(side=TOP)
+
 videoplayer.play()
 mixer.music.play()
 
@@ -31,6 +50,32 @@ def pause_play():
         mixer.music.pause()
         videoplayer.pause()
 
+def updateVolumeMeter(currVolume):
+    if (int(currVolume) < 1):
+        volume_meter.config(image=vol0)
+    elif (int(currVolume) > 0 and int(currVolume) <= 25):
+        volume_meter.config(image=vol1)
+    elif int(currVolume) >= 25 and int(currVolume) <= 50:
+        volume_meter.config(image=vol2)
+    elif int(currVolume) >= 50 and int(currVolume) <= 75:
+        volume_meter.config(image=vol3)
+    elif int(currVolume) >= 75 and int(currVolume) <= 100:
+        volume_meter.config(image=vol4)
+
+def volumeUp():
+    currVolume = mixer.music.get_volume()
+    mixer.music.set_volume(currVolume + 0.125)
+    print("Volume: ", currVolume)
+    updateVolumeMeter(currVolume*100)
+
+def volumeDown():
+    currVolume = mixer.music.get_volume()
+    mixer.music.set_volume(currVolume - 0.125)
+    print("Volume: ", currVolume)
+    updateVolumeMeter(currVolume*100)
+
+def closeTV():
+    window.destroy()
 
 # center this label
 #lbl1 = Label(window, text="Mind Mapper Sample Television", bg="black", fg="white", font="none 24 bold")
@@ -41,13 +86,13 @@ label.pack()
 pause_btn = Button(window, text='Pause', width=14, bg='red', fg='black', command=lambda: pause_play())
 pause_btn.pack()
 
-volUp_btn = Button(window, text='Volume Up', command=lambda: playAgain())
+volUp_btn = Button(window, text='Volume Up', command=lambda: volumeUp())
 volUp_btn.pack(side=TOP, pady=3)
 
-volDown_btn = Button(window, text='Volume Down', command=lambda: StopVideo())
+volDown_btn = Button(window, text='Volume Down', command=lambda: volumeDown())
 volDown_btn.pack(side=TOP, padx=4)
 
-menu_btn = Button(window, text='Menu', command=lambda: PauseVideo())
-menu_btn.pack(side=TOP, padx=5)
+exit_btn = Button(window, text='Exit', command=lambda: closeTV())
+exit_btn.pack(side=TOP, padx=5)
 
 window.mainloop()
